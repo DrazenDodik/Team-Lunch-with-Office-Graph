dec790c2169c66f474bb27460b6fd1caea03f49b
drazendodik <drazend@live.fi>
2019-06-08 19:30:26 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import { Client } from "@microsoft/microsoft-graph-client";
+
+// Initialize the MSAL @see https://github.com/AzureAD/microsoft-authentication-library-for-js/wiki/MSAL-basics#initialization-of-msal
+const userAgentApplication = new Msal.UserAgentApplication(clientId, undefined, callback, options);
+const authProvider = new MicrosoftGraph.MSALAuthenticationProvider(userAgentApplication, graphScopes);
+
+import { UserAgentApplication } from "msal";
+
+import { MSALAuthenticationProvider } from "./node_modules/@microsoft/microsoft-graph-client/lib/src/MSALAuthenticationProvider";
+
+const clientId = "d605fa00-82db-4d60-8f2a-403217e5cdfc"; // Client Id of the registered application
+const callback = (errorDesc, token, error, tokenType) => {};
+// An Optional options for initializing the MSAL @see https://github.com/AzureAD/microsoft-authentication-library-for-js/wiki/MSAL-basics#configuration-options
+const options = {
+	redirectUri: "http://localhost:3000/callback",
+};
+const graphScopes = ["user.read", "mail.send"]; // An array of graph scopes
+
+// Initialize the MSAL @see https://github.com/AzureAD/microsoft-authentication-library-for-js/wiki/MSAL-basics#initialization-of-msal
+const userAgentApplication = new UserAgentApplication(clientId, undefined, callback, options);
+const authProvider = new MSALAuthenticationProvider(userAgentApplication, scopes);
